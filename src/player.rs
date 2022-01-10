f369f2c1ca5574bcf151a3622615f2a7b2d591a4
Kieran Phillips <kieran.phillips@themarketinglab.co.uk>
2022-01-10 20:53:07 +0000

    Add Player render

--- src/player.rs
+++ src/player.rs
@@ -8,4 +8,14 @@ impl Player {
     pub fn new(position: Point) -> Self {
         Self { position }
     }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        ctx.set(
+            self.position.x,
+            self.position.y,
+            WHITE,
+            BLACK,
+            to_cp437('@'),
+        );
+    }
 }
