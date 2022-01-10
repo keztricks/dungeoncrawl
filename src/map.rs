6d808ae8dbe6c4f102c599e046d53e8b5e58380e
Kieran Phillips <kieran.phillips@themarketinglab.co.uk>
2022-01-10 20:40:05 +0000

    Add map implementation

--- src/map.rs
+++ src/map.rs
@@ -17,3 +17,15 @@ pub enum TileType {
     Wall,
     Floor,
 }
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+}
