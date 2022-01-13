890cd03396d5f63a56d433693000c3cf7e11b558
Kieran Phillips <kieran.phillips@themarketinglab.co.uk>
2022-01-13 20:30:07 +0000

    Move spawn_monster to correct file

--- src/components.rs
+++ src/components.rs
@@ -9,21 +9,6 @@ pub struct Player;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
-pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
-    ecs.push((
-        Enemy,
-        pos,
-        Render {
-            color: ColorPair::new(WHITE, BLACK),
-            glyph: match rng.range(0, 4) {
-                0 => to_cp437('E'),
-                1 => to_cp437('O'),
-                2 => to_cp437('o'),
-                _ => to_cp437('g'),
-            },
-        },
-    ));
-}
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct MovingRandomly;
