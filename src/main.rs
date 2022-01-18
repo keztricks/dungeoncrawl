f94906856a2c767f75c20c01ee67ce1c0bc4a833
Kieran Phillips <keztricks@gmail.com>
2022-01-18 21:11:41 +0000

    Sanitise prelude order

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,16 @@ mod systems;
 mod turn_state;
 
 mod prelude {
-    pub use bracket_lib::prelude::*;
-    pub use legion::systems::CommandBuffer;
-    pub use legion::world::SubWorld;
-    pub use legion::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+
+    pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
+
     pub use crate::camera::*;
     pub use crate::components::*;
     pub use crate::map::*;
